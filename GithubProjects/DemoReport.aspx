998bf8c26af3006a21a34bcc462acf867598a656
Logeshjztec <logesh@thejztec.com>
2025-04-30 15:16:54 +0530

    Report Updated

---
+++ GithubProjects/DemoReport.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DemoReport.aspx.cs" Inherits="DemoReport" %>
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Report</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+            
+            <rsweb:ReportViewer ID="ReportViewer1" runat="server" Height="1500px" Width="100%" ></rsweb:ReportViewer>
+        </div>
+    </form>
+</body>
+</html>
---
+++ GithubProjects/DemoReport.aspx.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DemoReport : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            bind();
+        }
+    }
+
+    public void bind()
+    {
+
+        try
+        {
+
+            DataTable table = new DataTable();
+            table.Columns.Add("rating", typeof(string));
+            table.Columns.Add("rcount", typeof(string));
+            table.Columns.Add("percentage", typeof(string));
+
+            table.Rows.Add("Complied", "1", "33");
+            table.Rows.Add("In Progress", "2", "33");
+            table.Rows.Add("Not Complied", "4", "34");
+
+            ReportViewer1.ProcessingMode = ProcessingMode.Local;
+            ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/DemoReport.rdlc");
+
+            ReportDataSource rds1 = new ReportDataSource("DataSet1", table);
+            ReportViewer1.LocalReport.DataSources.Add(rds1);
+
+            ReportViewer1.LocalReport.Refresh();
+
+        }
+        catch (Exception ex)
+        {
+            
+
+        }
+
+
+
+
+    }
+}
